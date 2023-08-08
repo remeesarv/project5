ff3c3fa2502314b5297e151946cbdf7e6ccbd512
jallubond <remeesarv155@gmail.com>
2023-08-08 13:39:10 +0530

    todo

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from .forms import TodoForm
+from .models import Task
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+class Tasklistview(ListView):
+    model=Task
+    template_name = 'index.html'
+    context_object_name = 'task'
+class Taskdetailview(DetailView):
+    model=Task
+    template_name = 'detail.html'
+    context_object_name = 'task'
+class Taskupdateview(UpdateView):
+    model = Task
+    template_name = 'edit.html'
+    context_object_name = 'task'
+    fields = ('taskname','priority','date')
+
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
+class Taskdeleteview(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('cbvhome')
+
+
+def add(request):
+    task = Task.objects.all()
+    if request.method == 'POST':
+        taskname=request.POST.get('taskname','')
+        priority=request.POST.get('priority','')
+        date = request.POST.get('date','')
+        user = Task(taskname=taskname,priority=priority,date=date)
+        user.save()
+    return render(request,'index.html',{'task':task})
+def delete(request,id):
+    task = Task.objects.get(id= id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    return render(request,'delete.html')
+def update(request,id):
+    task=Task.objects.get(id=id)
+    form = TodoForm(request.POST or None,instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'update.html',{'form':form,'task':task})
